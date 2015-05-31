4c753a210a3e0a133a63ca2ba3ebc86b14506846
Rokibul Hasan <rokibul@tasawr.com>
2015-05-31 18:56:42 +0600

    Send sms for activation requests

--- app/models/activation_call_request.rb
+++ app/models/activation_call_request.rb
@@ -2,6 +2,7 @@ class ActivationCallRequest < ActiveRecord::Base
 
   validate :validated_already_called
   before_validation :reset_attempt_count
+  after_save :send_message
 
   scope :by_imi_number, ->(imi_number) { where(imi_number: imi_number) }
   scope :by_cell_number, ->(cell_number) { where(cell_number: cell_number) }
@@ -24,6 +25,14 @@ class ActivationCallRequest < ActiveRecord::Base
 
   def reset_attempt_count
     requests.last.update_column(:attempt, attempt.to_i+1) if requests_exist?
+  end
+
+  def send_message
+    url = URI.parse("http://sms.nixtecsys.com/index.php?app=webservices&ta=pv&u=Rajiul.Karim&p=Rajiul123&to=#{self.cell_number}&msg=We+received+your+activation+request.")
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) { |http|
+      http.request(req)
+    }
   end
 
 end
