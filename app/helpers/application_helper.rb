b1212a68f6b714e13e3eaa76c3d00bd1be99ddff
rakib hasan <rakib063049@gmail.com>
2015-07-08 22:54:32 +0600

    Formatted date and time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def formated_date(data)
+    return "" if data.nil?
+    data.strftime("%b %d, %Y")
+  end
+
+  def formatted_datetime(data)
+    return "" if data.nil?
+    data.strftime("%b %d, %Y | %I:%M %p")
+  end
+
+  def custom_date_view(data)
+    return "" if data.nil?
+    data.strftime("%Y-%m-%d")
+  end
 end
