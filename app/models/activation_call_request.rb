03048c12228cf19369ecbaac4f932529e78d4276
Rokibul Hasan <rakib063049@gmail.com>
2017-10-25 15:12:05 +0600

    switch sms provider

--- app/models/activation_call_request.rb
+++ app/models/activation_call_request.rb
@@ -53,8 +53,9 @@ class ActivationCallRequest < ActiveRecord::Base
     msg = "Congratulations!+Your+are+eligible+to+receive+a+free+sample+of+#{self.project_name_str}"
     msg = "Already+Registered+in+#{self.project_name_str}" if previously_called?
     # url = URI.parse("http://sms.nixtecsys.com/index.php?app=webservices&ta=pv&u=Rajiul.Karim&p=Rajiul123&to=#{self.cell_number}&msg=#{msg}")
-    url = URI.parse("https://bplboxservice.banglaphone.net.bd/httpapi/sendsms?userId=miniaturesm&password=Dhaka1212&smsText=#{msg}&commaSeperatedReceiverNumbers=#{self.cell_number}")
+    url = URI.parse("http://powersms.banglaphone.net.bd/httpapi/sendsms?userId=miniaturesm&password=Dhaka1212&smsText=#{msg}&commaSeperatedReceiverNumbers=#{self.cell_number}")
     req = Net::HTTP::Get.new(url.to_s)
+    req.use_ssl = true if url.scheme == 'https'
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
     }
